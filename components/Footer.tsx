1c3286c4427a0483dfe90d1c40f481fa7470dd49
Prajwal A K <prajwalhassan7@gmail.com>
2023-12-14 08:54:32 +0530

    Footer Started 1.4

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,6 +25,7 @@ const Footer = () => {
             height={250}
           />
         </div>
+
         <div className="w-3/4 mt-2 h-40 bg-gradient-to-tr from-[#001f47] to-[#010a16] mx-auto rounded-lg shadow-lg shadow-neutral-600 hover:shadow-xl hover:shadow-neutral-700">
           <div className="flex justify-around my-16">
             <h1 className='text-white font-mono md:text-4xl sm:text-2xl my-auto'>
@@ -34,7 +35,7 @@ const Footer = () => {
               {
                 isSubscribed ? (
                   <Button onClick={toggleSubscribe} className='text-sm' variant={"primary"}>
-                    <BellRing  />
+                    <BellRing />
                     Subscribed
                   </Button>
                 ) : (
@@ -46,6 +47,42 @@ const Footer = () => {
               }
             </div>
           </div>
+        </div>
+
+        <div className="w-full px-16 md:px-32 mt-10">
+          <div className="flex max-sm:flex-col mx-auto  justify-around gap-2 items-center">
+            <h1 className='text-black md:text-2xl sm:text-lg my-auto hover:cursor-pointer hover:shadow-neutral-300' >
+              Home
+            </h1>
+            <h1 className='text-black md:text-2xl sm:text-lg my-auto hover:cursor-pointer hover:shadow-neutral-300' >
+              About
+            </h1>
+            <h1 className='text-black md:text-2xl sm:text-lg my-auto hover:cursor-pointer hover:shadow-neutral-300' >
+              Contact
+            </h1>
+            <h1 className='text-black md:text-2xl sm:text-lg my-auto hover:cursor-pointer hover:shadow-neutral-300' >
+              Services
+            </h1>
+          </div>
+
+          {/* <div className="flex justify-center mt-8">
+            <h1 className='text-black font-mono md:text-2xl sm:text-lg my-auto hover:cursor-pointer hover:shadow-neutral-300' >
+              Terms & Conditions
+            </h1>
+
+            <div className="">
+              Contact
+            </div>
+
+            <h1 className='text-black font-mono md:text-2xl sm:text-lg my-auto hover:cursor-pointer hover:shadow-neutral-300' >
+              Privacy Policy
+            </h1>
+
+            <h1 className='text-black font-mono md:text-2xl sm:text-lg my-auto hover:cursor-pointer hover:shadow-neutral-300' >
+              FAQ
+            </h1>
+
+          </div> */}
         </div>
       </div>
     </div >
